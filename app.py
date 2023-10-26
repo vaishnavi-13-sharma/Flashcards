a9debce8767962cd933853d8cca02a45ab870551
nav <fpd16@txstate,edu>
2023-10-26 15:45:16 -0500

    updated postman, code runs perfectly

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Configuration for SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Define the Card model
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(150), unique=True, nullable=False)
@@ -15,40 +17,55 @@ class Card(db.Model):
     def __repr__(self):
         return f"{self.question} - {self.ans}"
 
+# Route to add a new flashcard
 @app.route('/ques', methods=['POST'])
 def add_ques():
-    data = request.get_json()
-    ques = Card(question=data.get('ques'))
-    db.session.add(ques)
+    data = request.json
+    question = data.get('ques')
+    answer = data.get('ans')
+    card = Card(question=question, ans=answer)
+    db.session.add(card)
     db.session.commit()
-    return jsonify({'id': ques.id})
+    return {'id': card.id}
 
+# Route to read all flashcards with questions and answers
 @app.route('/ques', methods=['GET'])
 def read_ques():
-    ques = Card.query.all()
+    cards = Card.query.all()
     output = []
-    for q in ques:
-        ques_info = {'id': q.id, "ques": q.question, "ans": q.ans}
-        output.append(ques_info)
-    return jsonify({"ques": output})
+    for card in cards:
+        card_info = {
+            'id': card.id,
+            'ques': card.question,
+            'ans': card.ans
+        }
+        output.append(card_info)
+    return {'flashcards': output}
 
+# Route to update a flashcard (question and answer)
 @app.route('/ques/<int:id>', methods=['PUT'])
 def update_ques(id):
-    ques = Card.query.get_or_404(id)
-    data = request.get_json()
+    card = Card.query.get_or_404(id)
+    data = request.json
     if 'ques' in data:
-        ques.question = data['ques']
+        card.question = data['ques']
     if 'ans' in data:
-        ques.ans = data['ans']
+        card.ans = data['ans']
     db.session.commit()
-    return jsonify({"message": "Question updated", "id": ques.id, "ques": ques.question, "ans": ques.ans})
+    return {
+        "message": "Flashcard updated",
+        "id": card.id,
+        "ques": card.question,
+        "ans": card.ans
+    }
 
+# Route to delete a flashcard
 @app.route('/ques/<int:id>', methods=['DELETE'])
 def delete_ques(id):
-    ques = Card.query.get_or_404(id)
-    db.session.delete(ques)
+    card = Card.query.get_or_404(id)
+    db.session.delete(card)
     db.session.commit()
-    return jsonify({"message": "Question deleted"})
+    return {"message": "Flashcard deleted"}
 
 if __name__ == "__main":
     app.run(debug=True)
